f267787f73410ae3095e8a9847795b207f868ba8
Devon Langendoerfer <425devon@gmail.com>
2017-11-14 20:30:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object[key]=value
+  object[key]=value
+
+  return object
 }
