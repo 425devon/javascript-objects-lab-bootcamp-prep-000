0d0e18534ee7df9667922bf4dfd14c55bdb888a9
Devon Langendoerfer <425devon@gmail.com>
2017-11-14 20:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object)
   delete newObj.key;
   return newObj;
-  return object;
-}
+  }
 
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object.key
