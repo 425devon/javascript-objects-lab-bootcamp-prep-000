5783490f1cd4c20e5fb73fe829a7ffe86f6a4f89
Devon Langendoerfer <425devon@gmail.com>
2017-11-14 21:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var recipes = {};
 function updateObjectWithKeyAndValue(obj, key, value) {
 
   return Object.assign({}, obj, { [key]: value })
-}
+};
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value
 
   return object
-}
+};
 
 function deleteFromObjectByKey(object, key){
   var newObj =  Object.assign({}, object);
@@ -17,4 +17,4 @@ function deleteFromObjectByKey(object, key){
   delete newObj.key;
 
   return newObj;
-  }
+};
