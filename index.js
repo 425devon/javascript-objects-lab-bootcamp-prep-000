9e8c3f0a330297b5ac795fa69b69cdadfcb1157e
Devon Langendoerfer <425devon@gmail.com>
2017-11-14 20:26:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var recipes = {};
-var newRecipes = Object.assign({}, recipes)
 
-function updateObjectWithKeyAndValue(object, key, value){
-   object[key]=value;
-   return object;
+function updateObjectWithKeyAndValue(obj, key, value) {
+ 
+  return Object.assign({}, obj, { [key]: value })
 }
